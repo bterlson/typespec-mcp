adf0ada60ee593db0bd759087d63875b2a769bc9
Timothee Guerin <tiguerin@microsoft.com>
2025-05-07 16:02:25 -0700

    Tweaks

--- packages/typespec-mcp-http-server-js/src/emitter.tsx
+++ packages/typespec-mcp-http-server-js/src/emitter.tsx
@@ -1,22 +1,7 @@
-import { List } from "@alloy-js/core";
-import { FunctionDeclaration, SourceFile, VarDeclaration } from "@alloy-js/typescript";
 import { EmitContext } from "@typespec/compiler";
-import { writeOutput, Output } from "@typespec/emitter-framework";
-import { ToolsInterface } from "./components/ToolsInterface.jsx";
-import { mcpSdk } from "./externals/mcp-sdk.js";
-import { zod } from "typespec-zod";
-import { ZodTypes } from "./components/ZodTypes.jsx";
-import { ListToolsHandler } from "./components/ListToolsHandler.jsx";
-import { zodToJsonSchema } from "./externals/zodToJsonSchema.js";
-import { CallToolHandlers } from "./components/CallToolHandlers.jsx";
-import { createMCPServerContext, MCPServerContext } from "./context/McpServer.js";
-import { ServerDeclaration } from "./components/ServerDeclaration.jsx";
-import { ToolHandlerAccessors } from "./components/ToolHandlerAccessors.jsx";
-import { zodValidationError } from "./externals/zod-validation-error.js";
-import { TsTypes } from "./components/TsTypes.jsx";
+import { writeOutput } from "@typespec/emitter-framework";
+import { McpServer } from "typespec-mcp-server-js/components";
 
 export async function $onEmit(context: EmitContext) {
-  const mcpServerContext: MCPServerContext = createMCPServerContext(context.program);
-
-  const libs = [mcpSdk, zod, zodToJsonSchema, zodValidationError];
+  writeOutput(context.program, <McpServer program={context.program} />, context.emitterOutputDir);
 }
